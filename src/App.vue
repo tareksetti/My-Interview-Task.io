5028b1c8090aba69cdd2db28326c1781ef64fc0c
tareksetti <taraksetti@gmail.com>
2022-08-06 18:54:59 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="container">
-         <p class="text-center">Welcome to my interview task</p>
+         <p class="text-center">Welcome to tax calculator</p>
          <SalesTax />
        
 
